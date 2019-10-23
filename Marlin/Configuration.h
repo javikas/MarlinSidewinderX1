b8b81df05ab62ac0b8636d07f642b5f16a0eae77
javikas <javikas@gmail.com>
2019-10-24 00:15:48 +0200

    X probe offset adjustment

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -773,7 +773,7 @@
  *      O-- FRONT --+
  *    (0,0)
  */
-#define X_PROBE_OFFSET_FROM_EXTRUDER 32  // X offset: -left  +right  [of the nozzle]
+#define X_PROBE_OFFSET_FROM_EXTRUDER 31  // X offset: -left  +right  [of the nozzle]
 #define Y_PROBE_OFFSET_FROM_EXTRUDER -33 // Y offset: -front +behind [the nozzle]
 #define Z_PROBE_OFFSET_FROM_EXTRUDER 0   // Z offset: -below +above  [the nozzle]
 
